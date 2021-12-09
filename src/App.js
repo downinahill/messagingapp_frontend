1e05b55602f3f6f416e3ff885186c7ff7753c21d
downinahill <brian.hill.0023@gmail.com>
2021-12-08 20:19:13 -0500

    added pusher but might remove it. fixed a CORS problem. finally can get a message to show that i type but i have to refresh the page

--- src/App.js
+++ src/App.js
@@ -1,103 +1,152 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import Sidebar from './Components/Sidebar'
 import Chat from './Components/Chat'
+import axios from './Components/axios'
+import Nav from './Components/Nav'
+import Pusher from 'pusher-js'
 // import NewPost from './Components/NewPost'
 // import List from './Components/List'
-import Nav from './Components/Nav'
+
 // import Login from './Components/Login'
 
 let baseUrl = process.env.REACT_APP_BASEURL || "http://localhost:3003";
+axios.defaults.baseURL = baseUrl
+function App () {
+  const [messages, setMessages] = useState([])
+  useEffect(() => {
+      axios.get("messages/sync").then(res => {
+          setMessages(res.data)
+      })
+  }, [])
 
-class App extends Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    userData: [],
-    postData: [],
-  }
-}
-
-
-
-
-
-getPosts = () => {
-  // fetch
-  fetch(baseUrl + "/posts", {
-    credentials: "include"
-  })
-    .then((res) => {
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        return [];
-      }
+  useEffect(() => {
+    const pusher = new Pusher("9b5fb9e2d4497d2f068a", {
+      cluster: 'mt1'
     })
-    .then((data) => {
-      console.log(data);
-      this.setState({
-        postData: data,
-      });
-    });
-};
 
+    const channel = pusher.subscribe('messages')
+    channel.bind('inserted', (data) => {
+      setMessages([...messages, data])
+    })
 
+    return () => {
+      channel.unbind_all()
+      channel.unsubscribe()
+    }
+  }, [messages])
+  console.log(messages)
 
-  // Add Posts
-  // addPost = (newPost) => {
-  //   const copyGoals = [...this.state.postData];
-  //   copyGoals.push(newPost);
-  //   this.setState({
-  //     postData: data,
-  //   });
-  // };
-
-  //Edit Posts
-  handleEditedData = (data) => {
-    this.setState({
-      goalsData: data,
-    });
-  };
-
-  // Delete Posts
-  deletePost = (id) => {
-    fetch(baseUrl + "/posts/" + id, {
-      method: "DELETE",
-      credentials: "include"
-    }).then((res) => {
-      console.log(res);
-      const findIndex = this.state.goalsData.findIndex(
-        (goal) => goal._id === id
-      );
-      const copyGoals = [...this.state.goalsData];
-      copyGoals.splice(findIndex, 1);
-      this.setState({
-        goalsData: copyGoals,
-      });
-    });
-  };
-
-
-
-
-
-render() {
+  
   return (
-    <div className="App">
+    <div className="app">
       <div className="app_body">
         <Nav />
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
+        
       </div>
-      
     </div>
-  );
+  )
+
 }
-}
+
+export default App
 
 
 
 
 
-export default App;
+
+// class App extends Component {
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     userData: [],
+//     postData: [],
+//   }
+// }
+
+
+
+
+
+// getPosts = () => {
+//   // fetch
+//   fetch(baseUrl + "/posts", {
+//     credentials: "include"
+//   })
+//     .then((res) => {
+//       if (res.status === 200) {
+//         return res.json();
+//       } else {
+//         return [];
+//       }
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       this.setState({
+//         postData: data,
+//       });
+//     });
+// };
+
+
+
+//   // Add Posts
+//   // addPost = (newPost) => {
+//   //   const copyGoals = [...this.state.postData];
+//   //   copyGoals.push(newPost);
+//   //   this.setState({
+//   //     postData: data,
+//   //   });
+//   // };
+
+//   //Edit Posts
+//   handleEditedData = (data) => {
+//     this.setState({
+//       goalsData: data,
+//     });
+//   };
+
+//   // Delete Posts
+//   deletePost = (id) => {
+//     fetch(baseUrl + "/posts/" + id, {
+//       method: "DELETE",
+//       credentials: "include"
+//     }).then((res) => {
+//       console.log(res);
+//       const findIndex = this.state.postData.findIndex(
+//         (goal) => goal._id === id
+//       );
+//       const copyPosts = [...this.state.postsData];
+//       copyPosts.splice(findIndex, 1);
+//       this.setState({
+//         postsData: copyPosts,
+//       });
+//     });
+//   };
+
+
+
+
+
+// render() {
+//   return (
+//     <div className="App">
+//       <div className="app_body">
+//         <Nav />
+//         <Sidebar />
+//         <Chat messages={messages}/>
+//       </div>
+      
+//     </div>
+//   );
+// }
+// }
+
+
+
+
+
+// export default App;
--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,75 +1,183 @@
-import React, { useEffect, useState, Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, MoreVert, SearchOutlined, Send } from '@material-ui/icons';
 import './Chat.css';
-import axios from 'axios'
-import { Modal, Button } from "react-bootstrap";
+import MicIcon from '@material-ui/icons/Mic'
+import axios from './axios';
 
-class Chat extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            comment: [],
-            commentText: ""
-        }
+
+
+
+function Chat ({ messages }) {
+    const [seed, setSeed] = useState("")
+    const [input, setInput] = useState("")
+    const [text, setText] = useState("")
+    const sendMessage = async (e) => {
+        e.preventDefault()
+        await axios.post('/messages/new', { 
+            message: input,
+            name: "Brian",
+            timestamp: new Date().toUTCString(),
+            received: true
+        })
+        // setInput(sendMessage)
     }
-    addComment = (event) => {
-        event.preventDefault();
-
-    }
-
-    handleChange = (event) => {
-        this.setState({ commentText: event.target.value })
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000))
+    }, [])
 
 
-    }
-
-
-    render() {
-        console.log(this.state)
-        return (
-            <div className="chat">
-                <div className="chat_header">
-                    <Avatar />
-                    <div className="chat_headerInfo">
-                        <h3>Room Name</h3>
-                        <p>Last seen at...</p>
-                    </div>
-                    <div className="chat_headerRight">
-                        <IconButton>
-                            <SearchOutlined />
-                        </IconButton>
-                        <IconButton>
-                            <AttachFile />
-                        </IconButton>
-                        <IconButton>
-                            <MoreVert />
-                        </IconButton>
-                    </div>
+    return (
+        <div className="chat">
+            <div className="chat_header">
+                <Avatar src={'https://avatars.dicebear.com/api/human/b${seed}.svg'}/>
+                <div className="chat_headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at...</p>
                 </div>
-                <div className="chat_body">
-
-                </div>
-                <div className="chat_footer">
-                    <Send />
-
-                    <form onSubmit={this.addComment}>
-
-                        <input
-                            placeholder="What's goin on?"
-                            type="text"
-                            value={this.state.commentText}
-                            onChange={this.handleChange}
-                        />
-
-                        <button type="submit" >Send</button>
-                    </form>
-
+                <div className="chat_headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
                 </div>
             </div>
-        )
-    }
+            <div className="chat_body">
+            {messages.map(message => (
+                    <p className={`chat_message ${message.received &&
+                        'chat_receiver'}`}>
+                            <span className="chat_name">{message.name}</span>
+                                {message.message}
+                            <span className="chat_timestamp">
+                                {message.timestamp}
+                            </span>
+                    </p>
+                    
+                ))}
+
+                
+
+</div>
+            <div className="chat_footer">
+                <Send />
+
+                <form>
+
+                    <input
+                        placeholder="What's goin on?"
+                        type="text"
+                        value={input}
+                        onChange={e => setInput(e.target.value)}
+                    
+                    />
+                    <button onClick={sendMessage} type="submit">Send</button>
+                    
+                </form>
+                <MicIcon />
+
+            </div>
+        </div>
+    )
 }
 
 
+
 export default Chat
+
+
+
+
+// class Chat extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             comment: [],
+//             commentText: ""
+//         }
+//     }
+//     addComment = (event) => {
+//         event.preventDefault();
+
+//     }
+
+//     handleChange = (event) => {
+//         this.setState({ commentText: event.target.value })
+
+
+//     }
+
+
+//     render() {
+//         console.log(this.state)
+//         return (
+//             <div className="chat">
+//                 <div className="chat_header">
+//                     <Avatar />
+//                     <div className="chat_headerInfo">
+//                         <h3>Room Name</h3>
+//                         <p>Last seen at...</p>
+//                     </div>
+//                     <div className="chat_headerRight">
+//                         <IconButton>
+//                             <SearchOutlined />
+//                         </IconButton>
+//                         <IconButton>
+//                             <AttachFile />
+//                         </IconButton>
+//                         <IconButton>
+//                             <MoreVert />
+//                         </IconButton>
+//                     </div>
+//                 </div>
+//                 <div className="chat_body">
+//                     <p className="chat_message chat_receiver">
+//                         <span className="chat_name">Bob</span>
+//                         This is a message
+//                         <span className="chat_timestamp">
+//                             {new Date().toUTCString()}
+//                         </span>
+                        
+//                     </p>
+//                     <p className="chat_message">
+//                         <span className="chat_name">Brian</span>
+//                         This is a message back
+//                         <span className="chat_timestamp">
+//                             {new Date().toUTCString()}
+//                         </span>
+//                     </p>
+                    
+                    
+
+
+
+
+//                 </div>
+//                 <div className="chat_footer">
+//                     <Send />
+
+//                     <form onSubmit={this.addComment}>
+
+//                         <input
+//                             placeholder="What's goin on?"
+//                             type="text"
+//                             value={this.state.value}
+//                             onChange={this.handleChange}
+//                         />
+
+//                         <button type="submit" value="{this.state.commentText}" >Send</button>
+//                     </form>
+//                     <MicIcon />
+
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+
+// export default Chat
